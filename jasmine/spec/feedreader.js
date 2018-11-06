db467af78f208cf81c96d75c2dee55f4117e3047
Savoir <savoir.cn@gmail.com>
2018-11-05 23:23:07 -0800

    add read.me

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it("has a URL and the URL is defiend",function() {
+         it("has a URL and the URL is defined",function() {
             for (var i=0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url.length).not.toBe(0);
@@ -43,7 +43,7 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it("has a name and the names is defiend",function() {
+         it("has a name and the names is defined",function() {
             for (var i=0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(allFeeds[i].name.length).not.toBe(0);
